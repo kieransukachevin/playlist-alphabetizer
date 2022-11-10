a5c34c721274278a3c35414bcd0c0d463216db82
Kieran Sukachevin <kieran.sukachevin@gmail.com>
2022-11-10 15:49:24 -0800

    changes to name made

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SpotifyApp
+# Playlist Alphabetizer
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.11.
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  redirect_uri: 'https://kieransukachevin.github.io/spotify-alphabetize/login-redirect' // Your redirect uri
+  redirect_uri: 'https://kieransukachevin.github.io/playlist-alphabetizer/login-redirect' // Your redirect uri
 };
