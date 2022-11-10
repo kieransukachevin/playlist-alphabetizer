7ac32538b24a87e0e40c400e16bbf3a1672f15a9
Kieran Sukachevin <kieran.sukachevin@gmail.com>
2022-11-10 12:49:48 -0800

    changes made

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -30,4 +30,10 @@ export class PlaylistComponent implements OnInit {
   async alphabetize() {
     await this.spotifyService.alphabetizePlaylist();
   }
+
+  millisToMinutesAndSeconds(millis: number) {
+    var minutes = Math. floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000). toFixed(0);
+    return minutes + ':' + seconds;
+  }
 }
